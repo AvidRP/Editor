588033444b409b6098afae51c304ca1cc2ccf81e
AvidRP <s2pant@edu.uwaterloo.ca>
2017-08-23 00:02:16 -0600

    added menu bar with all common functions

--- editor.py
+++ editor.py
@@ -1,63 +1,100 @@
 from Tkinter import *
-from tkFileDialog import *
-from tkMessageBox import showerror
+#for saving purposes
+import tkFileDialog
+import tkMessageBox
 
-fileName = None
 
+#All functions
 def newFile():
-    global fileName
-    fileName = "New Untitled File"
-    #empty the file from 0 row and 0 column to the end of file to make new file
-    Text.delete(0.0, END)
-
-def saveAs():
-    #w for write for built in asksaveasfile
-    f = asksaveasfile(mode = 'w', defaultextension = '.txt')
-    t = Text.get(0.0, END)
-    #try catch to get rid of white space at the end of file when saved
-    try:
-        f.write(t.rstrip())
-    except:
-        showerror(title = "error", message = "Something went wrong. Unable to save file...")
-
-def saveFile():
-    global fileName
-    #storting the entire file contents in a var
-    t = Text.get(0.0, END)
-    f = open(fileName, 'w')
-    f.write(t)
-    f.close()
+    answer = tkMessageBox.askquestion(title="Save file", message="Do you want to save this file?")
+    #if yes first save then delete all
+    if(answer == True):
+        saveFile()
+    deleteAllOption()
 
 def openFile():
-    f = askopenfile(mode = 'r')
-    t = f.read()
-    #editor needs to be empty
-    Text.delete(0.0, END)
-    Text.insert(0.0, t)
+    newFile()
+    file = tkFileDialog.askopenfile()
+    text.insert(INSERT, file.read())
 
+def saveFile():
+    filePath = tkFileDialog.asksaveasfilename()
+    writeFile = open(filePath, mode='w')
+    writeFile.write(text.get(0.0, END).rstrip())
 
-#Working on the obj itself
+def closeFile():
+    saveFile()
+    root.quit()
+
+def selectAllOption():
+    #SELect all
+    text.tag_add(SEL, "0.0", END)
+
+def deleteAllOption():
+    text.delete(0.0, END)
+
+def cutOption():
+    #need to clear pre cut stuff
+    root.clipboard_clear()
+    root.clipboard_append(string=text.selection_get())
+    #delete first to last character
+    text.delete(index1=SEL_FIRST, index2=SEL_LAST)
+
+#same as cut without the deletion
+def copyOption():
+    root.clipboard_clear()
+    root.clipboard_append(string=text.selection_get())
+
+def pasteOption():
+    text.insert(INSERT, root.clipboard_get())
+
+def deleteOption():
+    text.delete(index1=SEL_FIRST, index2=SEL_LAST)
+
+#window object
 root = Tk()
 
-root.title("Hackable Text Editor")
-root.minsize(width=600, height = 600)
-root.maxsize(width = 600, height = 600)
+root.title("Untitled")
+#window size
+root.minsize(width=400, height=400)
 
-#so it fills up entire window
-text = Text(root, width = 600, height = 600)
-#need to actually display the text box
-text.pack()
+#setting up textBox
+#highlightthickness to get rid of border
+#for some reason undo function won't work unless set to trueasdf
+text= Text(root, width=400, height=400, font=("Arial"), bd =5, highlightthickness=0, undo=True)
 
-#working on the menu bar itself
-menuBar = Menu(root)
-fileMenu = Menu(menuBar)
-fileMenu.add_command(label="New", command = newFile)
-fileMenu.add_command(label="Open", command = openFile)
-fileMenu.add_command(label="Save", command = saveFile)
-fileMenu.add_command(label="Save As", command = saveAs)
+#so it takes up full screen when size is increased
+text.pack(fill=BOTH)
+
+
+#DROPDOWNS
+menu = Menu(root)
+root.config(menu=menu)
+
+#File Menu
+fileMenu = Menu(menu)
+menu.add_cascade(label="File", menu=fileMenu)
+fileMenu.add_command(label="New...", command=newFile)
+fileMenu.add_command(label="Open...", command=openFile)
 fileMenu.add_separator()
-fileMenu.add_command(label="Quit", command = root.quit)
-menuBar.add_cascade(label="File", menu =fileMenu)
+fileMenu.add_command(label="Save...", command=saveFile)
+fileMenu.add_separator()
+fileMenu.add_command(label="Close..", command=closeFile)
 
-root.config(menu=menuBar)
+#Edit Menu
+editMenu = Menu(menu)
+menu.add_cascade(label="Edit", menu=editMenu)
+editMenu.add_command(label="Undo", command=text.edit_undo)
+editMenu.add_command(label="Redo", command=text.edit_redo)
+editMenu.add_separator()
+editMenu.add_command(label="Cut", command=cutOption)
+editMenu.add_command(label="Copy", command=copyOption)
+editMenu.add_command(label="Paste", command=pasteOption)
+editMenu.add_command(label="Select All", command=selectAllOption)
+editMenu.add_separator()
+editMenu.add_command(label="Delete", command=deleteOption)
+
+#keep the editor running
 root.mainloop()
+
+
